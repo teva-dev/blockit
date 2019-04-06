daa8392f15062a2f9250c7b7739e25cfa3301a55
chevris <tevachevris@gmail.com>
2019-04-06 02:04:57 +0200

    commit 2

--- editor/blocks/section-column/edit.js
+++ editor/blocks/section-column/edit.js
@@ -23,11 +23,32 @@ const SectionColumnBlockEdit = ( props ) => {
 		columns
 	} = props.attributes;
 
+	// Pass an object to ResponsiveTabPanel to identify color icons ( ex : {sm: "#cccccc", lg: "#cccccc"} )
+	const iconsColor = {};
+	if ( blockitVariables && blockitVariables.media_sizes && Object.keys( blockitVariables.media_sizes ).length ) {
+		Object.keys( blockitVariables.media_sizes ).forEach( ( media ) => {
+			let sizeName = 'size';
+			let orderName = 'order';
+			let verticalAlignName = 'verticalAlign';
+
+			if ( media !== 'all' ) {
+					sizeName = `${ media }_${ sizeName }`; // sm_size
+					orderName = `${ media }_${ orderName }`; // sm_order
+					verticalAlignName = `${ media }_${ verticalAlignName }`; // sm_verticalAlign
+			}
+
+			// orderName and verticalAlignName always false if they are not responsive
+			if ( ! props.attributes[ sizeName ] && ! props.attributes[ orderName ] && ! props.attributes[ verticalAlignName ] ) {
+					iconsColor[ media ] = '#cccccc';
+			}
+		} );
+	}
+
 	return (
 		<Fragment>
 			<InspectorControls>
 				<PanelBody>
-					<ResponsiveTabPanel>
+					<ResponsiveTabPanel iconsColor={ iconsColor }>
 						{
 							( tab ) => <p>{ tab.title }</p>
 						}
--- editor/components/responsive-tab-panel/index.js
+++ editor/components/responsive-tab-panel/index.js
@@ -1,25 +1,30 @@
+import './editor.scss';
+
 /**
  * Internal dependencies
  */
-import { tabsMobile } from '../../icons/icons.js';
-import { tabsTablet } from '../../icons/icons.js';
-import { tabsLaptop } from '../../icons/icons.js';
-import { tabsDesktop } from '../../icons/icons.js';
-import { tabsTv } from '../../icons/icons.js';
+import { tabsMobile } from './icons.js';
+import { tabsTablet } from './icons.js';
+import { tabsLaptop } from './icons.js';
+import { tabsDesktop } from './icons.js';
+import { tabsTv } from './icons.js';
 
 /**
  * WordPress dependencies
  */
 const { blockitVariables } = window;
-const { __ } = wp.i18n;
+const { __, sprintf } = wp.i18n;
 const {	TabPanel, Icon, Tooltip } = wp.components;
 
 const ResponsiveTabPanel = ( props ) => {
 
 	const {
-		children
+		iconsColor = {},
+		children,
+		activeClass = 'is-active',
+		orientation = 'horizontal',
 	} = props;
-
+console.log(props)
 	if ( ! blockitVariables || ! blockitVariables.media_sizes || ! Object.keys( blockitVariables.media_sizes ).length ) {
 		return __( 'No media sizes found.' );
 	}
@@ -30,7 +35,7 @@ const ResponsiveTabPanel = ( props ) => {
 		tabsTablet,
 		tabsLaptop,
 		tabsDesktop,
-		tabsTv,
+		// tabsTv,
 	];
 
 	Object.keys( blockitVariables.media_sizes ).forEach( ( mediaName, i ) => {
@@ -38,18 +43,34 @@ const ResponsiveTabPanel = ( props ) => {
 			name: mediaName,
 			title: (
 				<Tooltip text={ sprintf( __( 'Devices with screen width <= %s' ), `${ blockitVariables.media_sizes[ mediaName ] }px` ) }>
-					<span>
+					<span style={ iconsColor && iconsColor[ mediaName ] ? {
+						color: iconsColor[ mediaName ],
+					} : {} }>
 						<Icon icon={ icons[i] } />
 					</span>
 				</Tooltip>
 			),
+			className: 'blockit-control-tabs-tab',
 		} );
+	} );
+	tabs.unshift( {
+		name: 'all',
+		title: (
+			<Tooltip text={ __( 'All devices' ) }>
+				<span>
+					<Icon icon={ tabsTv } />
+				</span>
+			</Tooltip>
+		),
+		className: 'blockit-control-tabs-tab',
 	} );
 
 	return (
 		<TabPanel
 			className="blockit-control-tabs"
 			tabs={ tabs }
+			activeClass={ activeClass }
+			orientation={ orientation }
 		>
 			{ children }
 		</TabPanel>
